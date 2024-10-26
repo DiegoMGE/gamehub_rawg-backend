5fe15b8f0524a59639eeb415eca9ff27dfa964ed
Diego Orellana <divigore@pm.me>
2024-10-26 14:58:03 -0600

    Endpoint added for fetching Genres.

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,29 @@ app.use(cors());
 
 const API_KEY = process.env.GAME_API_KEY;
 const API_URL = process.env.GAME_API_URL;
-const URL = `${API_URL}/games?key=${API_KEY}`;
+const URL_GAMES = `${API_URL}/games?key=${API_KEY}`;
+const URL_GENRES = `${API_URL}/genres?key=${API_KEY}`;
 
 app.get('/games', async (req, res) => {
   console.log('GET /games route hit');
   try {
-    const response = await axios.get(URL);
+    const response = await axios.get(URL_GAMES);
+
+    console.log("URL del servidor:", URL_GAMES);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.get('/genres', async (req, res) => {
+  console.log('GET /games route hit');
+  try {
+    const response = await axios.get(URL_GENRES);
+
+    console.log("URL del servidor:", URL_GENRES);
 
     res.json(response.data);
   } catch (error) {
