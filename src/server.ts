9753f4e38d8dc783e1af979ed50e6cc06fea6176
Diego Orellana <divigore@pm.me>
2024-10-28 22:11:11 -0600

    Endpoint added for list of platforms.

--- src/server.ts
+++ src/server.ts
@@ -12,9 +12,9 @@ const API_KEY = process.env.GAME_API_KEY;
 const API_URL = process.env.GAME_API_URL;
 const URL_GAMES = `${API_URL}/games?key=${API_KEY}`;
 const URL_GENRES = `${API_URL}/genres?key=${API_KEY}`;
+const URL_PLATFORMS = `${API_URL}/platforms/lists/parents?key=${API_KEY}`;
 
 app.get('/games', async (req, res) => {
-  console.log('GET /games route hit');
   try {
     const response = await axios.get(URL_GAMES);
 
@@ -28,11 +28,23 @@ app.get('/games', async (req, res) => {
 });
 
 app.get('/genres', async (req, res) => {
-  console.log('GET /games route hit');
   try {
     const response = await axios.get(URL_GENRES);
 
     console.log("URL del servidor:", URL_GENRES);
+
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+app.get('/platforms/lists/parents', async (req, res) => {
+  try {
+    const response = await axios.get(URL_PLATFORMS);
+
+    console.log("URL del servidor:", URL_PLATFORMS);
 
     res.json(response.data);
   } catch (error) {
